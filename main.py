e213e3a26dd272bd1381a84b88e743ad45b0ab0a
crased <hutchersongabriel9@gmail.com>
2026-04-01 13:14:29 -0400

    fixed working outside tool dir issue might remove shell script

--- main.py
+++ main.py
@@ -1,27 +1,40 @@
+import os
 import subprocess
+
 def find_save_game():
+ raw_dir = input("Enter folder name: ") 
+ if raw_dir is not None and raw_dir.strip() != "":
   try:
-   game = input("Enter gameTitle: ")
-   cmdPipe = subprocess.Popen(['find', '.', '-type', 'd'], stdout=subprocess.PIPE)
-   cmd = ['grep' , '-i', game]
-   result =  subprocess.Popen(cmd, stdin=cmdPipe.stdout, stdout=subprocess.PIPE, text=True)
-   cmdPipe.stdout.close()
+   pattern = f"*{raw_dir}*"
+   # search root changes the level the search starts at rn its starting in home directory
+   search_root = os.path.expanduser("~/Games")
+   cmd = ['find', search_root, '-type', 'd', '-iname', f"*{raw_dir}*"]
+   result =  subprocess.Popen(cmd, stdin=subprocess.PIPE, stdout=subprocess.PIPE, text=True)
    output, error = result.communicate()
-   dir_path = output.strip()
-   return dir_path.splitlines()[0]
-  except:
-   print("output is None")
+   if output.strip():
+      paths = output.strip().splitlines()
+      return paths[0]
+   else:
+      print(f"No folder matching '{raw_dir}' found in {search_root}")
+      return None   
+  except Exception as e:
+   print(f"Error has occurred: {e}")
    return None
+ print("Error: Game title cannot be empty.")
+ return None
 def search_for_save():
    try: 
-    output = find_save_game()
-    search_pattern = "save"
-    cmd = ['find', output, '-iname', f'*{search_pattern}']
-    result = subprocess.run(cmd, capture_output=True, text=True)
-    formated = result.stdout
-    print(formated)
+    path = find_save_game()
+    search_pattern = r".*\(sav\|dat\|profile\|conf\).*"
+    cmd = ['find', path, '-regextype', "sed", "-regex", search_pattern]
+    result = subprocess.run(cmd, capture_output=True, text=True, check=True)
+    if len(result.stdout) == 0:
+       print("cannot find save folder :{")
+       print("add folder name to search_pattern")
+    print(result.stdout)
+
    except Exception as e:
-    print(f"No directory found: {e}")
+    print(f"No directory found: {e} ")
     return     
 def main():
    search_for_save()
